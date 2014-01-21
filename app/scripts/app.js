c31cca49f94ed8f7bfa7a61ee50af6b1649319dd
Ethan Welborn <welborn.ethan@gmail.com>
2014-01-21 00:25:36 -0700

    initial commit with angularjs setup

---
+++ app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var app = angular.module('ethanwelborncomApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/home.html',
+        controller: 'HomeCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/work', {
+        templateUrl: 'views/work.html',
+        controller: 'WorkCtrl'
+      })
+      .when('/projects', {
+        templateUrl: 'views/projects.html',
+        controller: 'ProjectsCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/work.js
@@ -0,0 +1,10 @@
+'use strict';
+
+app
+  .controller('WorkCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
